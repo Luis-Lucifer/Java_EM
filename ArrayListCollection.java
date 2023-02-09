ccc9ce3ac6b99354155e69bde7711e4bd7ec72a2
Luis-Lucifer <nilanjandas5498@gmail.com>
2023-02-09 15:33:59 +0530

    Initial commit V1

---
+++ ArrayListCollection.java
@@ -0,0 +1,10 @@
+// import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ArrayListCollection {
+    public static void main(String[] args) throws Exception {
+        Collection<Object> data = new ArrayList<Object>();
+        
+    }
+}
---
+++ DividedByZero.java
@@ -0,0 +1,13 @@
+public class DividedByZero {
+    public static void main(String[] args) {
+        int i,j,k=0;
+        i = 12;
+        j= 0;
+        try {
+            k = i/j;
+        } catch (ArithmeticException e) {
+            System.out.println("Can't divide by zero");
+            System.out.println(k);
+        }
+    }
+}
